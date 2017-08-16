3a25e6aa2f865c280e1efaeb03555f5b6e4ec1ea
Kuba Wolanin <hi@kubawolanin.com>
2017-08-16 20:00:48 +0200

    [WIP] Language Server Protocol support
    LSP cleanup, final touches
    
    Signed-off-by: Kuba Wolanin <hi@kubawolanin.com>

---
+++ src/LanguageClient/LanguageClientProvider.ts
@@ -0,0 +1,57 @@
+import * as net from 'net'
+
+import {
+    Disposable,
+    workspace
+} from 'vscode'
+
+import {
+    StreamInfo,
+    LanguageClient,
+    LanguageClientOptions
+} from 'vscode-languageclient'
+
+export class LanguageClientProvider {
+    constructor() {
+    }
+
+    public connect(): Disposable {
+        let config = workspace.getConfiguration('openhab')
+        let connectionInfo = {
+            host: config.host,
+            port: config.lspPort
+        }
+
+        let extensions = [
+            'things',
+            'items',
+            'rules',
+            'script',
+            'sitemap',
+            'persist'
+        ]
+
+        /**
+         * Connect to language server via socket
+         */
+        let serverOptions = () => {
+            let socket = net.connect(connectionInfo);
+            let result: StreamInfo = {
+                writer: socket,
+                reader: socket
+            }
+            return Promise.resolve(result)
+        }
+
+        let clientOptions: LanguageClientOptions = {
+            documentSelector: ['openhab'],
+            synchronize: {
+                configurationSection: 'openhabLSP',
+                fileEvents: workspace.createFileSystemWatcher('**/*.{' + extensions.join(',') + '}')
+            }
+        }
+
+        let lc = new LanguageClient('openHABlsp', 'openHAB Server', serverOptions, clientOptions)
+        return lc.start()
+    }
+}
